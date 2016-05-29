545656e66e435eeac159996eb40bfb392f7f8097
motoki-shun <motoki-shun@kayac.com>
2016-05-29 09:50:46 +0900

    add SoundController.js

---
+++ src/SoundController.js
@@ -0,0 +1,104 @@
+import Cookies from 'js-cookie';
+
+export default class SoundController {
+    constructor(opts = {}) {
+        
+        this.COOKIE_NAME = opts.COOKIE_NAME || "isMute";
+        this.EXPIRES_DATE = opts.EXPIRES_DATE;
+        this.disableBlurPause = !!opts.disableBlurPause;
+        this.toggleSelector = opts.toggleSelector;
+        
+        this.SE = {};
+        
+        this.audio = new Audio(opts.src);
+        this.audio.volume = 0.2;
+        this.audio.loop = !opts.disableLoop;
+        
+        this.initListeners();
+    }
+    
+    initListeners() {
+        $(this.toggleSelector).on('click', () => {
+            this.toggleMute().pause().play();
+        });
+
+        if (!this.disableBlurPause) {
+            $(window).on('blur', () => {
+                this.pause();
+            }).on('focus', () => {
+                this.play();
+            });
+        }
+    }
+    
+    play() {
+        if (this.isMute) return this;
+        this.audio.play();
+        return this;
+    }
+    
+    pause() {
+        this.audio.pause();
+        return this;
+    }
+    
+    togglePlay(flag) {
+        if (flag === undefined) {
+            flag = !this.audio.paused;
+        }
+        flag ? this.play() : this.pause();
+        return this;
+    }
+    
+    mute() {
+        this.isMute = true;
+        this.audio.muted = true;
+        this.setCookie(this.isMute);
+        return this;
+    }
+    
+    unmute() {
+        this.isMute = false;
+        this.audio.muted = false;
+        this.setCookie(this.isMute);
+        return this;
+    }
+    
+    toggleMute(flag) {
+        if (flag === undefined) {
+            flag = !this.getCookie();
+        }
+        flag ? this.mute() : this.unmute();
+        return this;
+    }
+    
+    setSE(key, src) {
+        // if (Const.IS_TOUCH) return this;
+        this.SE[key] = new Audio(src);
+        return this;
+    }
+    
+    playSE(key) {
+        if (this.isMute) return this;
+        // if (Const.IS_TOUCH) return this;
+        
+        let se = this.SE[key];
+        if (!se) return this;
+        
+        se.play();
+        return this;
+    }
+    
+    setCookie(value) {
+        return Cookies.set(this.COOKIE_NAME, value, { expires: this.EXPIRES_DATE });
+    }
+    
+    getCookie() {
+        let value = Cookies.get(this.COOKIE_NAME);
+        return (value === "true");
+    }
+    
+    hasCookie() {
+        return Cookies.get(this.COOKIE_NAME) !== undefined;
+    }
+}
