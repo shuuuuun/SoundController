8dbc116984dd6ab3c94dd33b1c36c05b86381fc7
motoki-shun <motoki-shun@kayac.com>
2016-05-29 09:48:59 +0900

    update

--- config/browserify.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  entries: [
-    'src/js/main.js'
-  ],
-  dest: 'script.js'
-};
---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+const DEST_PATH = './build/';
+const SRC_PATH = './src/';
+const GLOB_PATH = `${SRC_PATH}**/*.js`;
+const GLOB_UNBUILD = `!${SRC_PATH}**/_**`;
+
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+const plumber = require('gulp-plumber');
+const uglify = require('gulp-uglify');
+const rename = require('gulp-rename');
+
+
+// tasks
+gulp.task('default', ['watch', 'build']);
+gulp.task('build', ['js', 'js-min']);
+
+gulp.task('watch', () => {
+  gulp.watch([GLOB_PATH], ['build']);
+});
+
+gulp.task('js', () => {
+  // minifyしない
+  gulp.src([GLOB_PATH, GLOB_UNBUILD])
+    .pipe(plumber())
+    .pipe(babel())
+    .pipe(gulp.dest(DEST_PATH));
+});
+
+gulp.task('js-min', () => {
+  // minifyする
+  gulp.src([GLOB_PATH, GLOB_UNBUILD])
+    .pipe(plumber())
+    .pipe(babel())
+    .pipe(uglify({ preserveComments: 'some' }))
+    .pipe(rename((path) => {
+      path.basename += '.min';
+    }))
+    .pipe(gulp.dest(DEST_PATH));
+});
